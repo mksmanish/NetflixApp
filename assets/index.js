3490d99eec17fa40191aea37e3e0df769c73d345
Manish Kumar <manish.kumar@tradesocio.com>
2023-09-19 14:04:22 +0530

    implemenation of register and plan screen

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,8 @@
 export const netflix = require('./images/Netflix.png');
+export const check = require('./images/checks.png');
+export const nlogo = require('./icons/logoN.png');
+export const cellphone = require('./icons/cellphone.png');
+export const laptop = require('./icons/laptop.png');
+export const tv = require('./icons/tv.png');
+export const tablet = require('./icons/tablet.png');
+export const television = require('./icons/television.png');
