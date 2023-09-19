cdb2ff22e66643226f67d22447b068cc1cf11d5b
Manish Kumar <manish.kumar@tradesocio.com>
2023-09-19 11:33:30 +0530

    adding screens

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import StackNavigator from './StackNavigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+      <StackNavigator />
+      <StatusBar style="light" />
+    </>
   );
 };
 
